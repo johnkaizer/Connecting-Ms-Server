2ee31cc915dd829ba5c5be48977e81012e8ca725
john kaiser <52598487+johnkaizer@users.noreply.github.com>
2023-01-15 23:19:46 +0300

    SignUp

---
+++ app/src/main/java/com/project/ms_server_connection/Connection/ConnectionClass.java
@@ -0,0 +1,8 @@
+package com.project.ms_server_connection.Connection;
+
+public class ConnectionClass {
+    public static String ip = "192.168.112.244";
+    public static String un = "JOHNTEZ\\kayza";
+    public static String pass = "Mylifeisgood20@";
+    public static String db =  "TestDatabase";
+}
--- app/src/main/java/com/project/ms_server_connection/SignUp.java
+++ app/src/main/java/com/project/ms_server_connection/SignUp.java
@@ -1,27 +1,114 @@
 package com.project.ms_server_connection;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.project.ms_server_connection.Connection.ConnectionClass;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 
 public class SignUp extends AppCompatActivity {
+    EditText EdName,EdEmail,EdPass;
+    AppCompatButton submitBtn;
+    TextView statusTxt;
+    Connection con;
+    Statement stmt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        EdEmail = findViewById(R.id.editText3);
+        EdName = findViewById(R.id.editText2);
+        EdPass = findViewById(R.id.editText5);
+        submitBtn = findViewById(R.id.appCompatButton);
+        statusTxt = findViewById(R.id.status_txt);
+
+        //register btn
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SignUp.registerUser().execute("");
+
+            }
+        });
+
+    }
+    public class registerUser extends AsyncTask<String, String,String >{
+        String z = "";
+        Boolean isSuccess = false;
+
+        @Override
+        protected void onPreExecute() {
+            statusTxt.setText("Sending data to database");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            statusTxt.setText("Registration successful");
+            EdName.setText("");
+            EdEmail.setText("");
+            EdPass.setText("");
+            startActivity(new Intent(SignUp.this, SignIn.class));
+            finish();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                con = connectionClass(ConnectionClass.un,ConnectionClass.pass,ConnectionClass.db,ConnectionClass.ip);
+                if (con == null){
+                    z= "check your internet connection";
+                }else {
+                    String sql = "INSERT INTO register (name,email,password) VALUES ('"+EdName.getText()+"','"+EdEmail.getText()+"','"+EdPass.getText()+"')";
+                    stmt = con.createStatement();
+                    stmt.executeUpdate(sql);
+
+                }
+
+            }catch (Exception e){
+                isSuccess = false;
+                z=e.getMessage();
+
+            }
+            return z;
+        }
     }
 
+    @SuppressLint("NewApi")
+    public Connection connectionClass(String user, String password, String database,String server){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionUrl = null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionUrl = "jdbc:jtds:sqlserver://" + server+"/" + database + ";user=" + user + ";password=" + password +"+";
+            connection = DriverManager.getConnection(connectionUrl);
+
+        }catch (Exception e){
+            Log.e("SQL Connection Error : ", e.getMessage());
+
+        }
+        return connection;
+    }
     public void sign_In(View view) {
         startActivity(new Intent(SignUp.this, SignIn.class));
-        finish();
-
-    }
-
-    public void home(View view) {
-        startActivity(new Intent(SignUp.this, MainActivity.class));
         finish();
 
     }
